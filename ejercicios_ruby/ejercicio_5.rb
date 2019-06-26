c2c734b35ae3a19e30cb20546f8ba79224c4a50a
Juan Diaz <judiazp2p@gmail.com>
2019-06-26 16:19:19 +0200

    ej5-resuelto

---
+++ ejercicios_ruby/ejercicio_5.rb
@@ -0,0 +1,11 @@
+#Escriba el pseudocódigo y el código Ruby para calcular el área de un círculo, dado su diámetro
+
+a = a
+PI = 3.14
+
+puts 'Ingrese el valor del diametro del circulo: '
+d = gets.to_i
+
+a = (PI / 4) * d**2
+
+puts "El area del circulo es: #{a}"
