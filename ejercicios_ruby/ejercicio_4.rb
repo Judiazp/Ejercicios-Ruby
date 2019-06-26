224d4f48ed7c2a5ec705d3f43c5a90af88f8d95c
Juan Diaz <judiazp2p@gmail.com>
2019-06-26 16:09:16 +0200

    ej4resuelto

---
+++ ejercicios_ruby/ejercicio_4.rb
@@ -0,0 +1,12 @@
+#Escriba el pseudocódigo y el código Ruby para calcular el área de un círculo, dado su radio
+
+a = a
+PI = 3.14
+
+puts 'Ingrese el valor del radio del circulo: '
+
+r = gets.to_i
+
+a = PI * r**2
+
+puts "El area del circulo es: #{a}"
