1c7614e6a0852e1ae6d08110c4dc13296c134280
Juan Diaz <judiazp2p@gmail.com>
2019-06-26 15:49:41 +0200

    ej3-resuelto

---
+++ ejercicios_ruby/ejercicio_3.rb
@@ -0,0 +1,15 @@
+#Escriba el código Ruby que calcule el área de un triángulo.
+
+a = a
+
+puts 'Ingrese la longitud de la base del triangulo: '
+
+b = gets.to_i
+
+puts 'Ingrese la longitud de la altura del triangulo: '
+
+h = gets.to_i
+
+a = b * h / 2
+
+puts "El area del triangulo es: #{a}"
