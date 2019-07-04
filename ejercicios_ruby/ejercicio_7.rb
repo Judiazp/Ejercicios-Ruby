dd2a17a9eebd03f4664e76ee8eb2d520de2dc697
Juan Diaz <judiazp2p@gmail.com>
2019-07-04 14:21:39 +0200

    ej7resuelto

---
+++ ejercicios_ruby/ejercicio_7.rb
@@ -0,0 +1,11 @@
+#Escriba el código Ruby que reciba un número entero, deduzca si es par y retorne el valor lógico según el caso.
+
+puts 'Ingrese un numero entero: '
+ e = gets.to_i
+if (e. even?)
+  puts 'par'
+  puts e. even?
+  else
+  puts 'Impar'
+  puts e. odd?
+end
