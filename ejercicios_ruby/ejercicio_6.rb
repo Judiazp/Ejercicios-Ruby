52460d07ea8f47c5387d92592c965fb69d309301
Juan Diaz <judiazp2p@gmail.com>
2019-07-04 13:08:45 +0200

    ej6resuelto

---
+++ ejercicios_ruby/ejercicio_6.rb
@@ -0,0 +1,13 @@
+#Escriba el código Ruby que calcule la suma de los 50 primeros números naturales.
+
+
+contador = 1
+
+
+for i in(0..49)
+
+  puts "#{i} + #{contador} = #{contador}"
+
+  contador = contador + 1
+
+end
