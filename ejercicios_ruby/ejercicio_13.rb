2d1a3118e88001ea526b30b1c47b2b72a70b65fd
Juan Diaz <judiazp2p@gmail.com>
2019-07-07 11:54:09 +0200

    ej5entrega-1

---
+++ ejercicios_ruby/ejercicio_13.rb
@@ -0,0 +1,12 @@
+#18Escriba el código Ruby que, dado un número, devuelva su factorial.
+
+contador = 0
+
+puts 'Ingrese un numero: '
+n = gets.to_i
+for i in 2..n
+  contador = contador + 1
+  n = n * contador
+end
+puts n
+
