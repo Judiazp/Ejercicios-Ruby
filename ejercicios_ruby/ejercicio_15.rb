43c3f47a60de61b7721c53ada99e3ce93d362fe3
Juan Diaz <judiazp2p@gmail.com>
2019-07-28 11:19:09 +0200

    entrega3-ej1

--- ejercicios_ruby/ejercicio_15.rb
+++ ejercicios_ruby/ejercicio_15.rb
@@ -2,21 +2,18 @@
 
 puts 'Ingrese un numero: '
 n = gets.to_i
-divisor = 2
-numeros_primos = 0
-while divisor <= n-1
-  r = n % divisor
-  if r == 0
-    #puts 'No es primo'
-    #break
-    if divisor == n-1
-     #puts 'Si es primo'
-      #break
-
+numeros_primos = -1
+for i in 1..n
+  j = i
+  contador = 0
+  for j in 1..i
+    if i % j == 0
+      contador = contador + 1
     end
-    numeros_primos = numeros_primos + 1
   end
-  divisor = divisor + 1
+    if contador > 2
+     else
+       numeros_primos += 1
+    end
 end
-
 puts numeros_primos
