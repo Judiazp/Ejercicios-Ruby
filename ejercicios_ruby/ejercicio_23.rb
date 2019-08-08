c5c21a37785e4be34b9f54720c3612548bf9e587
Juan Diaz <judiazp2p@gmail.com>
2019-08-08 21:48:00 +0200

    ejercicio-12

--- ejercicios_ruby/ejercicio_23.rb
+++ ejercicios_ruby/ejercicio_23.rb
@@ -2,19 +2,23 @@
 #el arreglo con sus valores ordenados de menor a mayor.
 
 arreglo = (1..5).to_a
-
+numero_mayor = 0
 for i in 0..arreglo.length-1
   arreglo[i] = rand(5)
   print arreglo[i]
 end
-aux=0
-for j in 0..arreglo[i]-1
-  if arreglo[j] < arreglo[j+1]
-    aux=arreglo[i]
-    arreglo[i]=arreglo[j+1]
-    arreglo[j+1]=aux
-
+aux = []
+ numero_mayor = 0
+ numero_menor = 0
+for j in 0..arreglo[i]
+  if arreglo[j] > numero_mayor
+    numero_mayor = arreglo[j]
+  elsif arreglo[j] < numero_menor
+    numero_menor = arreglo[j]
   end
+end
+puts
+puts numero_menor
+puts numero_mayor
 
 
-puts arreglo
