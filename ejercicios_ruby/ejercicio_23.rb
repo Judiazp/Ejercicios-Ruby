e7fe6d0b31c8452a29f95bf1ef4fc11cf42a5139
Juan Diaz <judiazp2p@gmail.com>
2019-08-12 23:32:38 +0200

    ejercicio-de-ordenamiento

--- ejercicios_ruby/ejercicio_23.rb
+++ ejercicios_ruby/ejercicio_23.rb
@@ -1,24 +1,24 @@
 #28Escriba el código Ruby que genere un arreglo con 20 números enteros aleatorios y devuelva
 #el arreglo con sus valores ordenados de menor a mayor.
 
-arreglo = (1..5).to_a
-numero_mayor = 0
-for i in 0..arreglo.length-1
-  arreglo[i] = rand(5)
-  print arreglo[i]
+arreglo = []
+
+for i in 0..5-1
+  arreglo << rand(5)+1
 end
-aux = []
- numero_mayor = 0
- numero_menor = 0
-for j in 0..arreglo[i]
-  if arreglo[j] > numero_mayor
-    numero_mayor = arreglo[j]
-  elsif arreglo[j] < numero_menor
-    numero_menor = arreglo[j]
+
+puts
+print arreglo
+puts
+aux = 0
+for j in 0..arreglo.length-1
+  if j != arreglo.length-1
+    if arreglo[j] <= arreglo[j+1]
+      aux = arreglo[j]
+      arreglo[j] = arreglo[j+1]
+      arreglo[j+1] = aux
+    end
   end
 end
 puts
-puts numero_menor
-puts numero_mayor
-
-
+print arreglo
