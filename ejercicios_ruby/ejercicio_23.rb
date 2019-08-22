a7faab4b6012ee9a765173431b62652031b326d7
Juan Diaz <judiazp2p@gmail.com>
2019-08-22 08:31:34 +0200

    ejercicio_de_ordenamiento_menor_a_mayor

--- ejercicios_ruby/ejercicio_23.rb
+++ ejercicios_ruby/ejercicio_23.rb
@@ -3,20 +3,22 @@
 
 arreglo = []
 
-for i in 0..5-1
-  arreglo << rand(5)+1
+for i in 0..20-1
+  arreglo << rand(20)+1
 end
 
 puts
 print arreglo
 puts
 aux = 0
-for j in 0..arreglo.length-1
-  if j != arreglo.length-1
-    if arreglo[j] <= arreglo[j+1]
-      aux = arreglo[j]
-      arreglo[j] = arreglo[j+1]
-      arreglo[j+1] = aux
+for h in 0..arreglo.length
+  for j in 0..arreglo.length-1
+    if j != arreglo.length-1
+      if arreglo[j] >= arreglo[j+1]
+        aux = arreglo[j]
+        arreglo[j] = arreglo[j+1]
+        arreglo[j+1] = aux
+      end
     end
   end
 end
